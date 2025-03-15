e3d65660ee243feade9b71018d64fbdd0c3aa085
Lee Roy Stevenson <leroysheep@gmail.com>
2025-03-16 00:29:02 +0100

    added data templates and changed app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open('storage/data.json', 'r', encoding="utf8") as handler:
+        blog_posts = json.load(handler)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
