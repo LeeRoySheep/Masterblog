e9ec445fc4cc875b176011fdf74c5340be79a0d4
Lee Roy Stevenson <leroysheep@gmail.com>
2025-03-16 12:22:00 +0100

    updated index and added a add template to add new posts.

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, request, redirect, url_for
+from storage_handler import StorageHandler
 
 app = Flask(__name__)
-
+storage = StorageHandler('storage/data.json')
 
 @app.route('/')
 def index():
-    with open('storage/data.json', 'r', encoding="utf8") as handler:
-        blog_posts = json.load(handler)
+    blog_posts = storage.comments
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blog_posts = storage.comments
+        new_post = dict()
+        new_post["id"] = (len(blog_posts)+1)
+        new_post["title"] = request.form.get('title')
+        new_post["author"] = request.form.get('name')
+        new_post["content"] = request.form.get('comment')
+        blog_posts.append(new_post)
+        storage.comments = blog_posts
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
---
+++ storage_handler.py
@@ -0,0 +1,54 @@
+import json
+
+class StorageHandler():
+    """
+    class to handle storage of blog comments or any other json storage
+    """
+    def __init__(self, file_path):
+        """
+        constructor to create instance of StorageHandler class
+        :param file_path: as string
+        :return: None
+        """
+        self._file_path = file_path
+
+
+    @property
+    def file_path(self):
+        """
+        getter to return file path
+        :return: self._file_path
+        """
+        return self._file_path
+
+
+    @file_path.setter
+    def file_path(self, new_file_path):
+        """
+        setter to overwrite file path
+        :param new_file_path: as string
+        :return: None
+        """
+        self._file_path = new_file_path
+
+
+    @property
+    def comments(self):
+        """
+        method to create a list of comments
+        :return: list of comments
+        """
+        with open(self.file_path, 'r', encoding="utf8") as handler:
+            return json.load(handler)
+
+
+    @comments.setter
+    def comments(self, new_comments):
+        """
+        method to add a comment to the list of comments
+        :param: new_comments: as list
+        :return: None
+        """
+        with open(self.file_path, 'w', encoding="utf8") as json_writer:
+            json.dump(new_comments, json_writer, indent=4)
+        return None
