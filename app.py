b82433857d281d983247916baec383db78014f57
Lee Roy Stevenson <leroysheep@gmail.com>
2025-03-18 03:06:14 +0100

    Last commit2

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def internal_server_error():
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
